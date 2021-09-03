4dca62cb3d6ec249668c5711a40730a28a6bf611
vertinski <vertinski@inbox.lv>
2021-09-03 03:05:54 +0300

    Add toggle button for pausing the program
    Add toggle button for pausing the program. Add new button design assets and status switching UX.

--- copyrun.py
+++ copyrun.py
@@ -1,17 +1,14 @@
 
 '''
 This file is part of CopyRun.
-
     CopyRun is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
     CopyRun is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
     You should have received a copy of the GNU General Public License
     along with CopyRun.  If not, see <https://www.gnu.org/licenses/>.
 '''
@@ -22,7 +19,6 @@ WARNING!!
 USE AT YOUR OWN RISK! NO GUARANTIES IF ANYTHING GETS BROKEN.
 DO NOT COPY ANYTHING EXCEPT PYTHON CODE YOU HAVE REVIEWED!!
 FOR EDUCATIONAL AND INFORMATIONAL PURPOSES ONLY.
-
 This app executes Python code after it is copied in clipboard.
 It saves the contents of the clipboard in file "testscript.py" and executes it.
 It ONLY executes content which is different from previously copied contents.
@@ -32,9 +28,14 @@ that this app is active and running. It also writes status messages in terminal.
 
 This warning is a part of CopyRun app and should be included in code at all times!
 
-Written by Vertinski
+
+Information:
+
+CopyRun is written by Vertinski
+vertinski@inbox.lv
 
 '''
+
 
 
 import tkinter as tk
@@ -43,21 +44,16 @@ import time
 
 
 
-
-
-
 warning = """WARNING!!
 USE AT YOUR OWN RISK! NO GUARANTIES IF ANYTHING GETS BROKEN.
 DO NOT COPY ANYTHING EXCEPT PYTHON CODE YOU HAVE REVIEWED!!
 FOR EDUCATIONAL AND INFORMATIONAL PURPOSES ONLY.
-
 This app executes Python code after it is copied in clipboard.
 It saves the contents of the clipboard in file "testscript.py" and executes it.
 It ONLY executes content which is different from previously copied contents.
 The CopyRun app waits for the launched code to exit and then continues to work.
 At all times there should be a warning window visible, which indicates
 that this app is active and running. It also writes status messages in terminal.
-
 This warning is a part of CopyRun app and should be included in code at all times!"""
 
 print()
@@ -66,11 +62,36 @@ print()
 
 
 
+
+def toggle_active (tog_butt):
+    global app_active
+
+    if app_active == True:
+        app_active = False
+        tog_butt.config (image=off_image)    # change toggle button image to non-active
+        lbl2.config (fg='grey')
+        lbl3.config (fg='grey')
+
+    else:
+        app_active = True
+        tog_butt.config (image=on_image)    # change toggle button image to active
+        lbl2.config (fg='red')
+        lbl3.config (fg='red')
+
+    #return app_active     #return app status boolean
+
+
+
+tog_stat = 'Active'
+app_active = True
+
+
+
 def main():
 
     app = tk.Tk()     # create Tkinter instance
     app.title ("CopyRun")
-    app.geometry ("200x120")
+    app.geometry ("200x140")
     app.configure(bg='black')
     app.call ('wm', 'attributes', '.', '-topmost', True)
 
@@ -81,6 +102,8 @@ def main():
     lbl0 = tk.Label (app, text='       ', font=('Arial Bold', 8), fg='red', bg='black')
     lbl0.pack()
 
+    global lbl2, lbl3
+
     lbl2 = tk.Label (app, text='APP IS ACTIVE!!', font=('Arial Bold', 16), fg='red', bg='black')
     lbl2.pack()
 
@@ -90,9 +113,26 @@ def main():
     lbl4 = tk.Label (app, text='       ', font=('Arial Bold', 8), fg='red', bg='black')
     lbl4.pack()
 
+    global on_image, off_image
+
+    on_image = tk.PhotoImage(file='assets/toggle_button_green.png')
+    off_image = tk.PhotoImage(file='assets/toggle_button_grey.png')
+
+    #tog_image = on_image
+
+    tog_butt = tk.Button (app, bg='black', activebackground='black', image=on_image,
+                          command= lambda:toggle_active(tog_butt), highlightthickness=0, borderwidth=0)
+    tog_butt.pack (padx=10, side='left')
+
+
     # add exit button
-    btn = tk.Button (app, text='Exit', command=app.destroy)
-    btn.pack()
+    exit_image= tk.PhotoImage(file='assets/exit_button.png')
+
+    exit_btn = tk.Button (app, bg='black', activebackground='black', image=exit_image,
+                          command=app.destroy, highlightthickness=0, borderwidth=0)
+    #exit_btn['activebackground'] = 'black'
+    exit_btn.pack (padx = 10, side='right')
+
 
 
     old_clip = app.clipboard_get()   # previously copied buffer for comparison
@@ -102,18 +142,24 @@ def main():
     #app.clipboard_append ('')    # clear existing clipboard
     #app.update()
 
-
     while True:
-        print ('CopyRun app is active and running.........')
+        if app_active == True:
+            print ('CopyRun app is active and running.........')
+            clip_text = app.clipboard_get()    # get clipboard text
+            time.sleep (0.2)
 
-        clip_text = app.clipboard_get()    # get clipboard text
-        time.sleep (0.5)
+            if clip_text != old_clip:           # if new text is copied in clipboard
+                with open ('testscript.py', 'w') as f:    # save to file and run it
+                    f.write (clip_text)
+                p = subprocess.run (['python3', 'testscript.py'])
+                old_clip = clip_text
 
-        if clip_text != old_clip:           # if new text is copied in clipboard
-            with open ('testscript.py', 'w') as f:    # save to file and run it
-                f.write (clip_text)
-            p = subprocess.run (['python3', 'testscript.py'])
-            old_clip = clip_text
+        else:
+            print ('CopyRun app is paused.........')
+            old_clip = app.clipboard_get()
+            time.sleep (0.2)
+
+        app.update()
 
 
 
@@ -126,5 +172,4 @@ if __name__ == "__main__":
         old_clip = ''      # clear the clipboard buffer
         clip_text = ''
         print('Exiting!')
-
 
